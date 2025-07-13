9dc5edc04d592b9ade0e67dee73cff620a8f1e9c
Malavika <malavika301@gmail.com>
2025-07-13 17:30:18 +0530

    docker updated

--- config/db.php
+++ config/db.php
@@ -6,10 +6,14 @@ $dbname = getenv("DB_NAME");
 $username = getenv("DB_USER");
 $password = getenv("DB_PASSWORD");
 
+echo "Connecting to host: " . $host;
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
+
+
 ?>
